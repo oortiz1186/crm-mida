fa5e806f7f7e5e5997c823360db4e5639395c520
Octavio Ortiz <157636731+oortiz1186@users.noreply.github.com>
2026-07-29 19:18:29 -0600

    feat(auth): add user role relation

---
+++ backend/src/CrmMida.Domain/Security/UserRole.cs
@@ -0,0 +1,18 @@
+namespace CrmMida.Domain.Security;
+
+public sealed class UserRole
+{
+    private UserRole() { }
+
+    public UserRole(Guid userId, Guid roleId)
+    {
+        UserId = userId;
+        RoleId = roleId;
+    }
+
+    public Guid UserId { get; private set; }
+    public User User { get; private set; } = null!;
+
+    public Guid RoleId { get; private set; }
+    public Role Role { get; private set; } = null!;
+}
