0c66d1d02e6bea4001e82f8affcb2c112a8bf20b
Octavio Ortiz <157636731+oortiz1186@users.noreply.github.com>
2026-07-31 11:19:27 -0600

    feat: route companies and prospects workspaces

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { BrowserRouter } from 'react-router-dom'
+import { Box, Button, CssBaseline, Stack, ThemeProvider } from '@mui/material'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import App from './App'
+import ProspectsApp from './ProspectsApp'
 import { theme } from './theme/theme'
+
+function Root() {
+  return (
+    <>
+      <Routes>
+        <Route path="/prospects" element={<ProspectsApp />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+      <Box position="fixed" bottom={16} right={16} zIndex={1600}>
+        <Stack direction="row" spacing={1}>
+          <Button component={Link} to="/" variant="outlined" size="small">Empresas</Button>
+          <Button component={Link} to="/prospects" variant="contained" size="small">Prospectos</Button>
+        </Stack>
+      </Box>
+    </>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-        <App />
+        <Root />
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
